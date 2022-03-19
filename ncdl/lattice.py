cb052fa5bee8efb405193d2b50c98f417e0a56a3
Joshua Horasek <joshua.horacsek@ucalgary.ca>
2022-03-18 22:50:14 -0600

    Stubbing out the structure of Lattice and Lattice tensors

---
+++ ncdl/lattice.py
@@ -0,0 +1,66 @@
+"""
+This is largely a rework of the ideas within some of the open research I did at Huawei
+in 2021. Most of the ideas in that work we're not well tested, and some of the implementation
+was a little sloppy (this is all on me).
+
+"""
+from ncdl.utils import get_coset_vector_from_name
+from typing import List, Tuple, Union
+import torch
+
+
+class LatticeTensor:
+    """
+    A LatticeTensor container abstracts the idea of storing information on a lattice
+    """
+    def __init__(self, lt: "LatticeTensor" = None):
+        if lt is not None:
+            print("copy?")
+        raise NotImplementedError()
+
+    def detach(self):
+        raise NotImplementedError()
+
+    def clone(self):
+        raise NotImplementedError()
+
+    def to(self, device):
+        raise NotImplementedError()
+
+    def is_tensor(self):
+        raise NotImplementedError()
+
+    def __cmp__(self, other):
+        raise NotImplementedError()
+
+    def __index__(self):
+        raise NotImplementedError()
+
+
+class Lattice:
+    """
+    The general "LatticeTensor" factory. Basically, this holds all the important information about the
+    point structure of the Lattice. LatticeTensors are instances of multi-dimensional sequences, but they
+    store values only within a bounded region (and on an integer lattice).
+
+
+    """
+    def __init__(self, input_lattice: Union[List, str], scale: Union[torch.FloatTensor, None] = None):
+        coset = input_lattice
+        if isinstance(input_lattice, str) and scale is None:
+            coset, scale = get_coset_vector_from_name(input_lattice)
+
+        if not isinstance(coset, list):
+            raise ValueError(f"The input 'input_lattice' should either be a common lattice identifier "
+                             f"or a list of IntTensors on the CPU that represent the coset structure "
+                             f"of the lattice. If you specified the latter case, then you should also "
+                             f"pass 'scale' as matrix (torch.IntTensor) that describes the scale of the "
+                             f"cosets.")
+
+        raise NotImplementedError()
+
+    def __cmp__(self, other):
+        raise NotImplementedError()
+
+    def __call__(self, *args):
+        raise NotImplementedError()
