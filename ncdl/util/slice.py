61b9632476d5415f61f15b44cc42ed1d67b5e164
Joshua Horacsek <joshua.horacsek@ucalgary.ca>
2023-03-09 08:01:27 -0800

    Fixing a bug in convolution when channels_out < channels_in

--- ncdl/util/slice.py
+++ ncdl/util/slice.py
@@ -19,7 +19,7 @@ def pad_or_slice_like_shape(tensor: torch._tensor, shape, mode='constant', value
     dim = len(shape) - 2
 
     # First, calculate the padding, then pad (if we to)
-    right_pad = [b - a for a, b in zip(tensor.shape[:2], shape[:2])]
+    right_pad = [b - a for a, b in zip(tensor.shape[2:], shape[2:])]
     if any([_ > 0 for _ in right_pad]):
 
         right_pad = [[0, 0] if _ < 0 else [_, 0] for _ in right_pad]
