e8bfb55fc7b7b3788b76e1e0660ae1a0e62320db
Joshua Horacsek <joshua.horacsek@ucalgary.ca>
2023-11-06 20:16:35 -0800

    Updating requirements for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../../ncdl'))
 extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
